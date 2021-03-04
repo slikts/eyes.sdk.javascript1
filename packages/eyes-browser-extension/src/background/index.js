d7d7b12bc61d9513cae4abc74f000df7f5408571
tourdedave <dave.haeffner@applitools.com>
2021-03-04 09:52:46 -0500

    bext: add visual-grid-client to the mix

--- packages/eyes-browser-extension/src/background/index.js
+++ packages/eyes-browser-extension/src/background/index.js
@@ -1,3 +1,6 @@
 process.hrtime = require('browser-process-hrtime')
 const {EyesSDK} = require('@applitools/eyes-sdk-core')
+const VisualGridClient = require('@applitools/visual-grid-client')
 console.log(EyesSDK)
+console.log(VisualGridClient)
+window.__eyes = EyesSDK
