6d3dfe54731c4ee8ee73806c8e5aa96ad6996755
tourdedave <dave.haeffner@applitools.com>
2021-03-04 10:29:38 -0500

    bext: instantiate Eyes in background script

--- packages/eyes-browser-extension/src/background/index.js
+++ packages/eyes-browser-extension/src/background/index.js
@@ -1,6 +1,11 @@
 process.hrtime = require('browser-process-hrtime')
 const {EyesSDK} = require('@applitools/eyes-sdk-core')
 const VisualGridClient = require('@applitools/visual-grid-client')
-console.log(EyesSDK)
-console.log(VisualGridClient)
-window.__eyes = EyesSDK
+const {version} = require('../../package.json')
+const {EyesFactory: Eyes} = EyesSDK({
+  name: 'eyes.browser-extension',
+  version,
+  spec: {},
+  VisualGridClient,
+})
+const eyes = new Eyes()
