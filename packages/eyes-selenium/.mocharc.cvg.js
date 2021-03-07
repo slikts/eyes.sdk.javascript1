cad7fff19ed6650a3f10356925d4777dd35df1e0
Amit Zur <amit.zur@applitools.com>
2021-03-07 17:29:58 +0200

    change cvg test concurrency to 50

--- packages/eyes-selenium/.mocharc.cvg.js
+++ packages/eyes-selenium/.mocharc.cvg.js
@@ -15,7 +15,7 @@ module.exports = {
     'node_modules/@applitools/sdk-shared/coverage-tests/custom/**/*.spec.js',
   ],
   parallel: true,
-  jobs: 65,
+  jobs: 50,
   timeout: 0,
   reporter: 'spec-xunit-file',
   require: ['./test/util/version-alias.js', '@applitools/sdk-shared/coverage-tests/util/mocha-hooks.js'],
