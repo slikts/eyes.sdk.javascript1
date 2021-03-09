632b71f10b97afdda2de2397ce4bd6647f06d899
tourdedave <dave.haeffner@applitools.com>
2021-03-09 07:59:06 -0500

    bext: improve logging in content script

--- packages/eyes-browser-extension/src/content/index.js
+++ packages/eyes-browser-extension/src/content/index.js
@@ -6,15 +6,14 @@ elementForInjectingScript.src = browser.runtime.getURL('/assets/pageScripts.js')
 window.document.body.appendChild(elementForInjectingScript)
 
 window.addEventListener('message', async event => {
-  console.log(`content script: called with ${JSON.stringify(event.data)}`)
   if (event.data && event.data.direction === 'from-page') {
+    console.log(`content script: called from page with ${JSON.stringify(event.data)}`)
     if (event.data.command === 'executeScript' && event.data.id) {
       window.__eyes.promises[event.data.id].res(event.data.result)
       delete window.__eyes.promises[event.data.id]
     } else {
       const result = await browser.runtime.sendMessage(event.data)
       console.log(`content script: received result from background script ${JSON.stringify(event.data)}`)
-      console.log('sending message to page')
       event.source.postMessage(result, '*')
     }
   }
@@ -26,12 +25,14 @@ window.__eyes = {
 
 browser.runtime.onMessage.addListener((message, _sender, sendResponse) => {
   if (message.direction === 'from-background-script' && message.command === 'executeScript') {
+    console.log(`content script: called from background-script with ${JSON.stringify(message)}`)
     const id = uuidv4()
     const p = new Promise((res, rej) => {
       window.__eyes.promises[id] = {res, rej}
     })
     window.postMessage({id, ...message}, '*')
     p.then(result => {
+      console.log(`content script: received result from page ${JSON.stringify(result)}`)
       return sendResponse(result)
     })
     return true
