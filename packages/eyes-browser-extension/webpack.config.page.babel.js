0b7d56ef0093ebbb6bde6e1d4bc2921d6f5c9adb
tourdedave <dave.haeffner@applitools.com>
2021-03-09 19:45:24 -0500

    bext: update webpack config of page-script so it runs in dev mode (to transpile debugger statements)

--- packages/eyes-browser-extension/webpack.config.page.babel.js
+++ packages/eyes-browser-extension/webpack.config.page.babel.js
@@ -1,6 +1,9 @@
 import path from 'path'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default {
+  mode: isProduction ? 'production' : 'development',
   context: path.resolve(__dirname, 'src'),
   entry: {
     pageHandler: './page/index.js',
