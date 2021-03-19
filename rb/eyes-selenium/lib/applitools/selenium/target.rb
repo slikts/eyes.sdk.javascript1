19369aa0ace7105bbc0e314c8eed40b7c3994a8b
tourdedave <dave.haeffner@applitools.com>
2021-03-19 06:58:38 -0400

    rb: refactor Target from a module into a class with static methods to ensure clean data on each use, also implement other methods needed in coverage tests (with remaining methods noted with skipped tests)

--- rb/eyes-selenium/lib/applitools/selenium/target.rb
+++ rb/eyes-selenium/lib/applitools/selenium/target.rb
@@ -1,8 +1,33 @@
 module Applitools
   module Selenium
-    module Target
-      extend self
-      @target = {}
+    class Target
+      def self.region(by, what)
+        new.region(by, what)
+      end
+
+      def self.fully(toggle)
+        new.fully(toggle)
+      end
+
+      def self.frame(input)
+        new.frame(input)
+      end
+
+      def self.window
+        new
+      end
+
+      def self.ignore_displacements(toggle)
+        new.ignore_displacements(toggle)
+      end
+
+      def self.ignore(by, what)
+        new.ignore(by, what)
+      end
+
+      def initialize
+        @target = {}
+      end
 
       def region(by, what)
         @target[:region] = {:type => by.to_s, :selector => what}
@@ -11,6 +36,23 @@ module Applitools
 
       def fully(toggle: true)
         @target[:isFully] = toggle
+        self
+      end
+
+      def frame(input)
+        @target[:frame] = [] if !@target[:frame]
+        @target[:frame] << input
+        self
+      end
+
+      def ignore_displacements(toggle)
+        @target[:ignoreDisplacements] = toggle
+        self
+      end
+
+      def ignore(by, what)
+        @target[:ignoreRegions] = [] if !@target[:ignoreRegions]
+        @target[:ignoreRegions] << {type: by.to_s, selector: what}
         self
       end
 
--- rb/eyes-selenium/spec/applitools/selenium/target_spec.rb
+++ rb/eyes-selenium/spec/applitools/selenium/target_spec.rb
@@ -9,4 +9,23 @@ describe 'Target' do
     result = ::Applitools::Selenium::Target.region(:css, 'div').fully
     expect(result.to_socket_output).to eql({:region => {:type => 'css', :selector => 'div'}, :isFully => true})
   end
+  it('frame') do
+    result = ::Applitools::Selenium::Target.frame('blah')
+    expect(result.to_socket_output).to eql({:frame => ['blah']})
+  end
+  it('window') do
+    result = ::Applitools::Selenium::Target.window
+    expect(result.to_socket_output).to eql({})
+  end
+  it('ignore_displacements') do
+    result = ::Applitools::Selenium::Target.ignore_displacements(true)
+    expect(result.to_socket_output).to eql({ignoreDisplacements: true})
+  end
+  it('ignore selector') do
+    result = ::Applitools::Selenium::Target.ignore(:css, '#overflowing-div')
+    expect(result.to_socket_output).to eql({ignoreRegions: [{type: 'css', selector: '#overflowing-div'}]})
+  end
+  it('ignore region') do
+    skip 'e.g., Target.ignore(Applitools::Region.new(50, 50, 100, 100))'
+  end
 end
