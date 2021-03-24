0d7f81a535b59dde2284bac7c613f016a10917d2
Tal Hayut <tal.hayut@applitools.com>
2021-03-24 15:14:41 +0200

    fix: pr comments

--- packages/sdk-shared/src/generate-dependabot-config.js
+++ packages/sdk-shared/src/generate-dependabot-config.js
@@ -2,17 +2,17 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const skipList = ['eyes-universal-poc', 'eyes-leanft', 'eyes-images-legacy', 'eyes-sdk-core-legacy'];
-let template = `  version: 2\n  updates:`;
 
 ;(async () => {
     const items = await fs.readdir(path.join(__dirname, '../../'));
     const packages = items.filter(package => !skipList.includes(package) && !package.startsWith('.'));
-    for (const package of packages) {
-        template += `
-  - package-ecosystem: "npm"
+    const packagesStr = packages
+    .filter(package => !skipList.includes(package) && !package.startsWith('.'))
+    .map(package => `  - package-ecosystem: "npm"
     directory: "/packages/${package}"
     schedule:
-      interval: "daily"`;
-    }
-    await fs.writeFile(path.join(__dirname,'../../../.github/dependabot.yml'), template);
+      interval: "daily"`)
+    .join('\n')
+    const fileContent = `version: 2\nupdates:\n${packagesStr}`
+    await fs.writeFile(path.join(__dirname,'../../../.github/dependabot.yml'), fileContent);
 })()
