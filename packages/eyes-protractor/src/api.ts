470fca354778fd7d0e9301e8ab0ad2dea57e8aeb
kyrylo <kyrylo.onufriiev@applitools.com>
2021-03-31 17:23:21 +0300

    protractor fix api

--- packages/eyes-protractor/src/api.ts
+++ packages/eyes-protractor/src/api.ts
@@ -16,7 +16,8 @@ export * from '@applitools/eyes-api'
 export {Driver, Element, Selector}
 
 export class Eyes extends api.Eyes<Driver, Element, Selector> {
-  protected readonly _spec = {...sdk, ...spec}
+  protected static readonly _spec = sdk
+  protected readonly _spec = sdk
   static setViewportSize: (driver: Driver, viewportSize: api.RectangleSize) => Promise<void>
 }
 
@@ -25,19 +26,20 @@ export type ClassicConfigurationPlain = api.ClassicConfigurationPlain<Element, S
 export type ConfigurationPlain = api.ConfigurationPlain<Element, Selector>
 
 export class Configuration extends api.Configuration<Element, Selector> {
-  protected readonly _spec = spec
+  protected static readonly _spec = sdk
+  protected readonly _spec = sdk
 }
 
 export type CheckSettingsPlain = api.CheckSettingsPlain<Element, Selector>
 
 export class CheckSettings extends api.CheckSettings<Element, Selector> {
-  protected readonly _spec = spec
+  protected static readonly _spec = sdk
+  protected readonly _spec = sdk
 }
 
 export const Target: api.Target<Element, Selector> = CheckSettings as any
 
-export const closeBatch = api.closeBatch(sdk)
-
 export class BatchClose extends api.BatchClose {
+  protected static readonly _spec = sdk
   protected readonly _spec = sdk
 }
