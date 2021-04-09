2dee80601240a7157805e6c05bc1c0ee07fff136
kyrylo <kyrylo.onufriiev@applitools.com>
2021-04-09 19:02:55 +0300

    add name formatting

--- .github/actions/setup-matrix/index.js
+++ .github/actions/setup-matrix/index.js
@@ -1,25 +1,25 @@
 const core = require('@actions/core')
 
 const ALIASES = {
-  'eyes-playwright': ['playwright', '@applitools/eyes-playwright'],
-  'eyes-puppeteer': ['pptr', 'puppeteer', '@applitools/eyes-puppeteer'],
-  'eyes-webdriverio-5': ['wdio', 'webdriverio', 'eyes-webdriverio', '@applitools/eyes-webdriverio'],
-  'eyes-webdriverio-4': ['wdio-legacy', 'webdriverio-legacy', 'eyes.webdriverio', '@applitools/eyes.webdriverio'],
-  'eyes-selenium': ['selenium', '@applitools/eyes-selenium'],
-  'eyes-protractor': ['protractor', '@applitools/eyes-protractor'],
-  'eyes-nightwatch': ['nightwatch', '@applitools/eyes-nightwatch'],
-  'eyes-testcafe': ['testcafe', '@applitools/eyes-testcafe'],
+  'playwright': ['eyes-playwright', '@applitools/eyes-playwright'],
+  'puppeteer': ['pptr', 'eyes-puppeteer', '@applitools/eyes-puppeteer'],
+  'webdriverio': ['wdio', 'eyes-webdriverio-5', 'eyes-webdriverio', '@applitools/eyes-webdriverio'],
+  'webdriverio-legacy': ['wdio-legacy', 'eyes-webdriverio-4', 'eyes.webdriverio', '@applitools/eyes.webdriverio'],
+  'selenium': ['eyes-selenium', '@applitools/eyes-selenium'],
+  'protractor': ['eyes-protractor', '@applitools/eyes-protractor'],
+  'nightwatch': ['eyes-nightwatch', '@applitools/eyes-nightwatch'],
+  'testcafe': ['eyes-testcafe', '@applitools/eyes-testcafe'],
 }
 
-const PREFIXES = {
-  'eyes-playwright': 'playwright',
-  'eyes-puppeteer': 'puppeteer',
-  'eyes-webdriverio-5': 'wdio',
-  'eyes-webdriverio-4': 'wdio_legacy',
-  'eyes-selenium': 'selenium',
-  'eyes-protractor': 'protractor',
-  'eyes-nightwatch': 'nightwatch',
-  'eyes-testcafe': 'testcafe',
+const DIR_NAME = {
+  'playwright': 'eyes-playwright',
+  'puppeteer': 'eyes-puppeteer',
+  'webdriverio': 'eyes-webdriverio-5',
+  'webdriverio-legacy': 'eyes-webdriverio-4',
+  'selenium': 'eyes-selenium',
+  'protractor': 'eyes-protractor',
+  'nightwatch': 'eyes-nightwatch',
+  'testcafe': 'eyes-testcafe',
 }
 
 const settings = core.getInput('settings', {required: true})
@@ -28,13 +28,15 @@ const include = settings.split(/[\s,]+/).reduce((output, setting) => {
   const [_, name, version, protocol] = setting.match(/^(.*?)(?:@(\d+))?(?::(.+?))?$/i)
   const package = Object.keys(ALIASES).find(dirname => dirname === name || ALIASES[dirname].includes(name))
   if (!package) return output
-  const prefix = PREFIXES[package].toUpperCase()
+  const modifiers = Object.entries({version, protocol})
+    .reduce((parts, [key, value]) => [...parts, `${key}: ${value}`], [])
+    .join('; ')
   output.push({
-    setting,
-    package,
+    name: `${package} ${modifiers ? `(${modifiers})` : ''}`,
+    package: DIR_NAME[package],
     env: {
-      [`APPLITOOLS_${prefix}_MAJOR_VERSION`]: version,
-      [`APPLITOOLS_${prefix}_PROTOCOL`]: protocol
+      [`APPLITOOLS_${package.toUpperCase()}_MAJOR_VERSION`]: version,
+      [`APPLITOOLS_${package.toUpperCase()}_PROTOCOL`]: protocol
     },
   })
   return output
