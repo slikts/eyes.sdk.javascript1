dd2273fc95d69bfaf99bc88aec3fa1a8a4e3e4f1
kyrylo <kyrylo.onufriiev@applitools.com>
2021-04-12 21:36:38 +0300

    fix internal error propagation

--- packages/eyes-sdk-core/lib/new/get-results.js
+++ packages/eyes-sdk-core/lib/new/get-results.js
@@ -1,7 +1,13 @@
 function makeGetResults({runner}) {
   return async function getResults() {
     const results = await runner.getAllTestResults(false)
-    return results.getAllResults().map(results => results.toJSON())
+    return results.getAllResults().map(results => {
+      const container = results.toJSON()
+      if (container.exception && container.exception.getTestResults && container.exception.getTestResults()) {
+        return {testResults: container.exception.getTestResults().toJSON(), exception: null}
+      }
+      return container
+    })
   }
 }
 
