82bbec88f1a07d03dda43a6782e6a41245365a87
kyrylo <kyrylo.onufriiev@applitools.com>
2021-05-19 16:36:23 +0300

    fix render and add gropping

---
+++ packages/test-utils/src/get-test-dom.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch')
+
+async function getTestDom(result, domId) {
+  const sessionUrl = new URL(result.apiUrls.session)
+  const accountId = sessionUrl.searchParams.get('accountId')
+  const url = `${sessionUrl.origin}/api/images/dom/${domId}/?accountId=${accountId}&apiKey=${process.env.APPLITOOLS_API_KEY_READ}`
+
+  const response = await fetch(url)
+  const dom = await response.json()
+
+  return {
+    ...dom,
+    getNodesByAttribute: attr => getNodesByAttribute(dom, attr),
+  }
+
+  function getNodesByAttribute(node, attr) {
+    const result = []
+    if (node.attributes && node.attributes[attr]) {
+      result.push(node)
+    }
+    if (node.childNodes) {
+      node.childNodes.forEach(node => result.push(...getNodesByAttribute(node, attr)))
+    }
+    return result
+  }
+}
+
+module.exports = getTestDom
--- packages/test-utils/src/get-test-info.js
+++ packages/test-utils/src/get-test-info.js
@@ -7,29 +7,4 @@ async function getTestInfo(result, apiKey = process.env.APPLITOOLS_API_KEY_SDK |
   return response.json()
 }
 
-async function getDom(result, domId) {
-  const sessionUrl = new URL(result.apiUrls.session)
-  const accountId = sessionUrl.searchParams.get('accountId')
-  const url = `${sessionUrl.origin}/api/images/dom/${domId}/?accountId=${accountId}&apiKey=${process.env.APPLITOOLS_API_KEY_READ}`
-
-  const response = await fetch(url)
-  const dom = await response.json()
-
-  return {
-    ...dom,
-    getNodesByAttribute: attr => getNodesByAttribute(dom, attr),
-  }
-
-  function getNodesByAttribute(node, attr) {
-    const result = []
-    if (node.attributes && node.attributes[attr]) {
-      result.push(node)
-    }
-    if (node.childNodes) {
-      node.childNodes.forEach(node => result.push(...getNodesByAttribute(node, attr)))
-    }
-    return result
-  }
-}
-
-module.exports = {getTestInfo, getDom}
+module.exports = getTestInfo
