1f812259a1fefeabfa67602a45513f9ab77271b8
kyrylo <kyrylo.onufriiev@applitools.com>
2021-05-19 13:26:41 +0300

    create scripts and test-utils packages

--- packages/test-utils/src/get-test-info.js
+++ packages/test-utils/src/get-test-info.js
@@ -1,20 +1,14 @@
 const fetch = require('node-fetch')
-const {URL} = require('url')
 
-async function getTestInfo(
-  testResults,
-  apiKey = process.env.APPLITOOLS_API_KEY_SDK || process.env.APPLITOOLS_API_KEY,
-) {
-  const url = `${testResults
-    .getApiUrls()
-    .getSession()}?format=json&AccessToken=${testResults.getSecretToken()}&apiKey=${apiKey}`
+async function getTestInfo(result, apiKey = process.env.APPLITOOLS_API_KEY_SDK || process.env.APPLITOOLS_API_KEY) {
+  const url = `${result.apiUrls.session}?format=json&AccessToken=${result.secretToken}&apiKey=${apiKey}`
 
   const response = await fetch(url)
   return response.json()
 }
 
-async function getDom(testResults, domId) {
-  const sessionUrl = new URL(testResults.getAppUrls().getSession())
+async function getDom(result, domId) {
+  const sessionUrl = new URL(result.apiUrls.session)
   const accountId = sessionUrl.searchParams.get('accountId')
   const url = `${sessionUrl.origin}/api/images/dom/${domId}/?accountId=${accountId}&apiKey=${process.env.APPLITOOLS_API_KEY_READ}`
 
