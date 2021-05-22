dbd33b96c120141ec3be32e87e2002f786d94eb3
Kyrylo <59825365+kyryloonufriiev@users.noreply.github.com>
2021-05-22 12:41:40 +0300

    Delete IOSVersion.ts

--- packages/eyes-api/src/enums/IOSVersion.ts
+++
@@ -1,8 +0,0 @@
-export enum IosVersionEnum {
-  LATEST = 'latest',
-  ONE_VERSION_BACK = 'latest-1',
-  /** @deprecated */
-  LATEST_ONE_VERSION_BACK = 'latest-1',
-}
-
-export type IosVersion = `${IosVersionEnum}`
