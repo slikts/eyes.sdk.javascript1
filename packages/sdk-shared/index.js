f02de7073ec6ffb7f14071f6759dc900ac0471ad
kyrylo <kyrylo.onufriiev@applitools.com>
2021-05-22 12:49:46 +0300

    fix shared

--- packages/sdk-shared/index.js
+++ packages/sdk-shared/index.js
@@ -1,5 +1,4 @@
 const {getTestInfo, getDom} = require('./src/get-test-info')
-const coverageTestsConfig = require('./coverage-tests/generic')
 
 module.exports = {
   getTestInfo,
@@ -9,5 +8,4 @@ module.exports = {
   testServer: require('./src/run-test-server'),
   processCommons: require('./src/process-commons'),
   ApiAssertions: require('./coverage-tests/util/ApiAssertions'),
-  coverageTestsConfig,
 }
