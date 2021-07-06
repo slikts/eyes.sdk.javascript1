6103ca9d608a75970486a2403c1a51a62914e5ef
kyrylo <kyrylo.onufriiev@applitools.com>
2021-07-06 23:57:56 +0300

    remove local emitter

--- packages/eyes-browser-extension/test/coverage/config.js
+++ packages/eyes-browser-extension/test/coverage/config.js
@@ -1,11 +1,9 @@
 module.exports = {
   extends: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/config.js',
-  emitter: '/Users/ep/Documents/applitools/sdk.coverage.tests/js/emitter.js',
   env: {
     NO_SDK: true,
     SPEC_DRIVER_PATH: './test/utils/spec-driver.js',
     SETUP_EYES_PATH: './test/utils/setup-eyes.js',
   },
   emitOnly: () => true,
-  
 }
