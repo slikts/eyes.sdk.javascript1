14248c625f66c2b65bc136d05023948739e03623
Tal Hayut <tal.hayut@applitools.com>
2021-08-11 13:35:49 +0300

    chore: merge conflicts

--- packages/eyes-selenium/src/spec-driver.ts
+++ packages/eyes-selenium/src/spec-driver.ts
@@ -5,7 +5,7 @@ import type * as Selenium from 'selenium-webdriver'
 export type Driver = Selenium.WebDriver
 export type Element = Selenium.WebElement
 export type Selector = Selenium.Locator | {using: string; value: string} | string | {type: string; selector: string}
-export type Cookie = Selenium.IWebDriverCookie
+
 // #region HELPERS
 
 const byHash = ['className', 'css', 'id', 'js', 'linkText', 'name', 'partialLinkText', 'tagName', 'xpath']
@@ -219,6 +219,7 @@ export async function waitUntilDisplayed(driver: Driver, selector: Selector, tim
   const element = await findElement(driver, selector)
   await driver.wait(until.elementIsVisible(element), timeout)
 }
+
 // #endregion
 
 // #region MOBILE COMMANDS
@@ -278,10 +279,7 @@ export async function build(env: any): Promise<[Driver, () => Promise<void>]> {
     appium = false,
     args = [],
     headless,
-  } = parseEnv({
-    ...env,
-    legacy: env.legacy ?? process.env.APPLITOOLS_SELENIUM_MAJOR_VERSION === '3',
-  })
+  } = parseEnv({...env, legacy: env.legacy ?? process.env.APPLITOOLS_SELENIUM_MAJOR_VERSION === '3'})
   const desiredCapabilities = {browserName: browser, ...capabilities}
   if (configurable) {
     const browserOptionsName = browserOptionsNames[browser || desiredCapabilities.browserName]
